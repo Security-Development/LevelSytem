4d5770df9fe1294763168eb2dc150068386ce102
Security-Development <35417717+Security-Development@users.noreply.github.com>
2022-03-08 08:40:45 +0900

    Update LevelEvent.php

--- Level/src/Neo/event/LevelEvent.php
+++ Level/src/Neo/event/LevelEvent.php
@@ -15,21 +15,26 @@ class LevelEvent implements Listener {
     }
 
     public function onJump(PlayerJumpEvent $event) : void {
-        LevelUtils::incXp($event->getPlayer(), 1);
-        $event->getPlayer()->sendMessage('xp 1획득');
+        LevelUtils::incXp($event->getPlayer(), 1250);
+        $event->getPlayer()->sendMessage('xp 1250획득');
     }
 
-    public function onLevelUp(LevelUpEvent $event) {
+    public function onLevelUp(LevelChangeEvent $event) {
         $player = $event->getPlayer();
+        if( $event->getAction() == LevelChangeEvent::UP ) {
+            $player->sendMessage("레벨업을 하셨군요~");
+        }
     }
 
     public function onXpChange(XpChangeEvent $event) : void {
         $player = $event->getPlayer();
         $data = Level::getData();
     
-        if( LevelUtils::getMaxXp($player) < $event->getNewXp() ) {
-            LevelUtils::LevelUp($player, 1);
-            LevelUtils::setXp($player, $event->getNewXp() / LevelUtils::getMaxXp($player));
+        if( LevelUtils::getMaxXp($player) <= $event->getNewXp() ) {
+            $level = round($event->getNewXp() / LevelUtils::getMaxXp($player));
+            LevelUtils::LevelUp($player, $level );
+            LevelUtils::setXp($player, $event->getNewXp() - (LevelUtils::getMaxXp($player) * $level ) );
+            LevelUtils::setMaxXp($player, ceil(5000 * LevelUtils::getLevel($player) * 2.7));
         }
         $player->getXpManager()->setXpLevel($data[$player->getName()]['level']);
         $player->getXpManager()->setXpProgress(LevelUtils::getXpPercent($player) / 100);
